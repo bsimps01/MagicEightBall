078d7f88257cfdb26d15d138a5a541f0add3871e
Ben Simpson <b.albertsimpson@gmail.com>
2019-11-19 16:26:11 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Benjamin Simpson on 11/19/19.
+//  Copyright © 2019 Benjamin Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
